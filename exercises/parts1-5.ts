9f4d29722ad98ad2cc879b08a227590016fa264d
leerohrer1 <leerohrer1@gmail.com>
2022-06-16 18:33:30 -0500

    Assignment completed.

--- exercises/parts1-5.ts
+++ exercises/parts1-5.ts
@@ -1,17 +1,22 @@
 // URL for the instructions: 
 // https://education.launchcode.org/intro-to-professional-web-dev/chapters/typescript/exercises.html 
-
+import { SpaceLocation } from "./SpaceLocation";
 
 // Part 1: Declare (5) Variables With Type
-
+// const spacecraftName: string = 'Determination';
+// const speedMph: number = 17500;
+const kilometersToMars: number = 225000000;
+const kilometersToTheMoon: number = 384400;
+// const milesPerKilometer: number = 0.621;
 
 
 // Part 2: Print Days to Mars
-
+// const milesToMars: number = kilometersToMars * milesPerKilometer;
+// const hoursToMars: number = milesToMars / speedMph;
+// const daysToMars: number = hoursToMars / 24;
 
 
 // Code an output statement here (use a template literal):
-
 
 
 // Part 3: Create a Function ("getDaysToLocation")
@@ -23,19 +28,39 @@
 
 
 
-
 // Part 4: Create a Spacecraft Class
+class Spacecraft {
+  milesPerKilometer: number = 0.621;
+  name: string;
+  speedMph: number;
 
+  constructor(name: string, speedMph: number) {
+    this.name = name;
+    this.speedMph = speedMph;
+  }
+
+  getDaysToLocation(kilometersAway: number): number {
+    const milesAway: number = kilometersAway * this.milesPerKilometer;
+    const hoursToLocation: number = milesAway / this.speedMph;
+    return hoursToLocation / 24;
+  }
+
+  printDaysToLocation(location: SpaceLocation) {
+    console.log(`${this.name} would take ${this.getDaysToLocation(location.kilometersAway)} days to get to ${location.name}.`);
+ }
+
+}
 
 
 
 // Create an instance of the class here:
-
+let spaceShuttle = new Spacecraft('Determination', 17500);
 
 
 // Move your output statements from part 3 here. Update the template literals use the
 // instance of the class.
-
+spaceShuttle.printDaysToLocation(new SpaceLocation('Mars', kilometersToMars));
+spaceShuttle.printDaysToLocation(new SpaceLocation('the Moon', kilometersToTheMoon));
 
 
 // Part 5: Export and Import the SpaceLocation Class
